8a93414bfd92cddb89cb45d58cccbc394b6572f1
Saurabh <67124630+Srbhsprstr@users.noreply.github.com>
2021-03-14 13:51:28 +0530

    Add files via upload
    It is personal browser with basic functionalities in which user can set home page and navigate accordingly and have access to various navigation bars.

---
+++ main.py
@@ -0,0 +1,50 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
+from PyQt5.QtWebEngineWidgets import *
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        #nav bar
+        navvar=QToolBar()
+        self.addToolBar(navvar)
+
+        back_button=QAction('Back',self)
+        back_button.triggered.connect(self.browser.back)
+        navvar.addAction(back_button)
+
+        forward_button=QAction('Forward',self)
+        forward_button.triggered.connect(self.browser.forward)
+        navvar.addAction((forward_button))
+
+        reload_button=QAction('Reload',self)
+        reload_button.triggered.connect(self.browser.reload)
+        navvar.addAction(reload_button)
+
+        home_button=QAction('Home',self)
+        home_button.triggered.connect(self.navigate_home)
+        navvar.addAction(home_button)
+
+        self.url_bar=QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navvar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://youtube.com'))
+    def navigate_to_url(self):
+        url=self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+    def update_url(self,q):
+        self.url_bar.setText(q.toString())
+
+app=QApplication(sys.argv)
+QApplication.setApplicationName("AntiChrome")
+window=MainWindow()
+app.exec_()
